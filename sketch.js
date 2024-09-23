87fb7d72dea8e4aa5d8715c1e56db7916286d344
ka-0602 <kauana.santos.barbosa@escola.pr.gov.br>
2024-09-23 10:08:19 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+function setup() {
+  createCanvas(400, 400);
+   background("black")
+}
+function draw() {
+  fill("white")
+    strokeWeight(1)
+  ellipse(200 , 120 , 80, 80)
+  ellipse(200, 200, 100, 100)
+  ellipse(200, 300, 150, 150)
+  
+  fill("black")
+  ellipse(188, 110, 10, 10)
+  ellipse(213, 110, 10, 10)
+   
+  fill(3, 3, 3)
+  ellipse(200, 180, 10, 10)
+  ellipse(200, 200, 10, 10)
+  ellipse(200, 220, 10, 10)
+  
+  
+  fill(245, 120, 17)
+  triangle(200, 130, 144, 133, 200, 119)
+  
+  strokeWeight(3)
+   stroke(191, 48, 48)
+  line(249, 196, 318,170)
+  line(152, 195, 65, 168)
+  
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+}
